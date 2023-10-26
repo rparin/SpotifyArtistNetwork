07af8672fbbb119c60ce241f96190fa5a34ac266
Ralph Parin <r.parin070@gmail.com>
2023-10-25 19:39:18 -0700

    create backend

---
+++ Server/routes/spotifyRouter.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const SpotifyAPI = require("../helpers/SpotifyAPI");
+const router = express.Router();
+
+//Import Env Variables
+require("dotenv").config();
+const { CLIENT_ID, CLIENT_SECRET } = process.env;
+
+//Create Helper Classes
+const spotifyAPI = new SpotifyAPI(CLIENT_ID, CLIENT_SECRET);
+
+router.get("/cAuthToken", async (req, res) => {
+  res.json({ access_token: await spotifyAPI.getClientAuthToken() });
+});
+
+// /relatedMap/:id/:authToken
+router.get("/relatedMap", async (req, res) => {
+  //   const id = req.params.id; //0bAsR2unSRpn6BQPEnNlZm
+  //   const authToken = req.params.authToken;
+  const id = "0bAsR2unSRpn6BQPEnNlZm"; //0bAsR2unSRpn6BQPEnNlZm
+  const authToken = await spotifyAPI.getClientAuthToken();
+  res.json({
+    relatedArtists: await spotifyAPI.getArtistRelatedMap(id, 1, authToken),
+  });
+});
+
+module.exports = router;
