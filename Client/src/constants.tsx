6029ee5c1687b6cda68f5d00cb7a964eeb3e6986
Ralph Parin <r.parin070@gmail.com>
2023-11-12 11:02:33 -0800

    Client: Create constants file

---
+++ Client/src/constants.tsx
@@ -0,0 +1,21 @@
+const APP_NAME = "Spotify Artist Network";
+
+const MY_NETWORK_BTN_TEXT = "View My Network";
+
+const SEARCH_PLACEHOLDER =
+  "Enter the name of an artist or provide a Spotify link";
+const SHORT_SEARCH_PLACEHOLDER = "Search Artist or enter Spotify link";
+
+const FOOTER_SEARCH_TEXT = "How search works";
+const FOOTER_LOGIN_TEXT = "Why login?";
+const CR = "©️ 2023. Developed by Ralph Parin.";
+
+export {
+  APP_NAME,
+  MY_NETWORK_BTN_TEXT,
+  SEARCH_PLACEHOLDER,
+  SHORT_SEARCH_PLACEHOLDER,
+  FOOTER_SEARCH_TEXT,
+  FOOTER_LOGIN_TEXT,
+  CR,
+};
