805b75af44e93ad60311c4cfe4bba499fd77f3bb
Ralph Parin <r.parin070@gmail.com>
2023-11-12 11:22:09 -0800

    npm i shadcn

--- Client/src/lib/utils.ts
+++ Client/src/lib/utils.ts
@@ -1,22 +1,6 @@
-// import { type ClassValue, clsx } from "clsx";
-// import { twMerge } from "tailwind-merge";
-
-// export function cn(...inputs: ClassValue[]) {
-//   return twMerge(clsx(inputs));
-// }
-
-export interface Dictionary<T> {
-  [Key: string]: T;
-}
-
-export interface Node {
-  id: string;
-  spotify_id: string;
-  img: string;
-  size: number;
-}
-
-export interface ClientToken {
-  access_token: string;
-  obtained_at: number;
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+ 
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
