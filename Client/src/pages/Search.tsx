cf22841dc29af0d37f3e670259912fe3c7b6fa21
Ralph Parin <r.parin070@gmail.com>
2023-11-12 20:10:24 -0800

    Client: Center text in search

--- Client/src/pages/Search.tsx
+++ Client/src/pages/Search.tsx
@@ -4,7 +4,7 @@ import Searchbox from "@/components/Searchbox";
 export default function Search() {
   return (
     <>
-      <div className="h-[70vh] flex flex-col justify-center items-center gap-7">
+      <div className="h-[70vh] flex flex-col justify-center items-center text-center gap-7">
         <h1 className="text-3xl md:text-5xl">{APP_NAME}</h1>
         <Searchbox />
       </div>
--- Client/src/pages/SearchResults.tsx
+++ Client/src/pages/SearchResults.tsx
@@ -3,7 +3,7 @@ import { APP_NAME } from "@/constants";
 export default function SearchResults() {
   return (
     <>
-      <div className="flex flex-col md:flex-row items-center gap-5 pt-10 md:pt-5 px-5">
+      <div className="flex flex-col md:flex-row text-center items-center gap-5 pt-10 md:pt-5 px-5">
         <h1 className="text-2xl">{APP_NAME}</h1>
         <Searchbox className="w-[90%] md:w-[50%] h-10" />
       </div>
