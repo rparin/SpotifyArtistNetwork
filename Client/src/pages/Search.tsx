598d88378b49a14b7c77ccc3196288fa6b32857c
Ralph Parin <r.parin070@gmail.com>
2023-11-13 16:27:35 -0800

    Client: Searchbox use lucide-react search icon

--- Client/src/pages/Search.tsx
+++ Client/src/pages/Search.tsx
@@ -1,12 +1,13 @@
 import { APP_NAME } from "@/constants";
 import { Searchbox } from "@/components/UI/Searchbox";
+import { SEARCH_PLACEHOLDER } from "@/constants";
 
 export default function Search() {
   return (
     <>
       <div className="h-[70vh] flex flex-col justify-center items-center text-center gap-7">
         <h1 className="text-3xl md:text-5xl">{APP_NAME}</h1>
-        <Searchbox />
+        <Searchbox placeholder={SEARCH_PLACEHOLDER} />
       </div>
     </>
   );
