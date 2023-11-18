a886c5a40c132bb77dfc672fc8ef0bda8f39559c
Ralph Parin <r.parin070@gmail.com>
2023-11-17 23:25:36 -0800

    Client: Change layout to display footer at the btm

--- Client/src/app/layout.tsx
+++ Client/src/app/layout.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from "@/components/ThemeProvider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -20,15 +21,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={cn(inter.className, "min-h-screen flex flex-col")}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange>
-          <Header />
-          {children}
-          {/* <Footer /> */}
+          <div className="flex-1">
+            <Header />
+            {children}
+          </div>
+          {/* Display footer at the bottom */}
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
