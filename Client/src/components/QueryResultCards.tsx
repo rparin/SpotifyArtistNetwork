e06ea78637cb0e19494dfd2225980d21f6d2a1da
Ralph Parin <r.parin070@gmail.com>
2023-11-18 20:31:16 -0800

    Client: Create infinite scroll

--- Client/src/components/QueryResultCards.tsx
+++ Client/src/components/QueryResultCards.tsx
@@ -1,15 +1,26 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import ResultArtistCard from "@/components/ResultArtistCard";
+import { useEffect, useState, useRef, useCallback, JSX } from "react";
+import { ResultArtistCard, artistCardType } from "./ResultArtistCard";
 import { useLocalStorage } from "@/hooks/LocalStorage";
 import { checkClientToken } from "@/lib/utils";
-import { fetchSearchResults } from "@/lib/API/Spotify/SpotifyAPI";
+import {
+  fetchSearchResults,
+  fetchNextPageSearchResults,
+} from "@/lib/API/Spotify/SpotifyAPI";
 
 export default function QueryResultCards(props: { query?: string }) {
   const { getItem, setItem, removeItem } = useLocalStorage("clientToken");
   const [searchResult, setSearchResult] = useState<null | any>(null);
-  const [pages, setPages] = useState<null | {}>(null);
+  const [nextPage, setNextPage] = useState<null | string>(null);
+  const [loading, setLoading] = useState(false);
+
+  const parseNextPage = (nextPage: string) => {
+    const searchParams = new URLSearchParams(nextPage);
+    return `query=${props.query}&type=artist&offset=${searchParams.get(
+      "offset"
+    )}&limit=${searchParams.get("limit")}`;
+  };
 
   const getToken = async () => {
     await checkClientToken(getItem()).then((data) => {
@@ -18,20 +29,69 @@ export default function QueryResultCards(props: { query?: string }) {
     return getItem().access_token;
   };
 
+  const setPage = (page: string | null) => {
+    if (page) {
+      setNextPage(parseNextPage(page));
+    } else {
+      setNextPage(null);
+    }
+  };
+
   useEffect(() => {
-    async function doWork() {
+    async function fetchData() {
+      setLoading(true);
       if (!props.query) return;
       const cToken = await getToken();
       const res = await fetchSearchResults(props.query, cToken);
-      const cards = res.data.artists.items.map((item: any, index: any) => {
-        let bakImage = "./noImage.png";
-        if (item?.images[0]?.url) {
-          bakImage = item.images[0].url;
-        }
+      setLoading(false);
+      if (res.error) return;
+      setPage(res.data.artists.next);
+      setSearchResult(res.data.artists.items);
+    }
 
-        return (
+    fetchData();
+  }, [props.query]);
+
+  const observer = useRef<any>();
+  const lastArtistCardRef = useCallback(
+    (node: HTMLElement) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      async function fetchData() {
+        const cToken = await getToken();
+        if (!nextPage) return;
+        const res = await fetchNextPageSearchResults(nextPage, cToken);
+        if (res.error) return;
+        setSearchResult((prev: any) => [...prev, ...res.data.artists.items]);
+        setPage(res.data.artists.next);
+      }
+
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+          setLoading(true);
+          fetchData();
+          setLoading(false);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading, nextPage]
+  );
+
+  const getArtistCards = () => {
+    if (!searchResult) return;
+    const idSet = new Set();
+    const cards: JSX.Element[] = [];
+    searchResult.map((item: any, index: any) => {
+      let bakImage = "./noImage.png";
+      if (item?.images[0]?.url) {
+        bakImage = item.images[0].url;
+      }
+      if (!idSet.has(item.id)) {
+        cards.push(
           <ResultArtistCard
-            key={item.id}
+            ref={searchResult.length === index + 1 ? lastArtistCardRef : null}
+            key={`${item.id}`}
             id={item.id}
             name={item.name}
             genres={item.genres}
@@ -42,18 +102,11 @@ export default function QueryResultCards(props: { query?: string }) {
             url={item.external_urls.spotify}
           />
         );
-      });
+        idSet.add(item.id);
+      }
+    });
+    return cards;
+  };
 
-      setSearchResult(cards);
-
-      setPages({
-        prev: res.data.artists.previous,
-        next: res.data.artists.next,
-      });
-    }
-
-    doWork();
-  }, [props.query]);
-
-  return <>{searchResult}</>;
+  return <>{getArtistCards()}</>;
 }
--- Client/src/components/ResultArtistCard.tsx
+++ Client/src/components/ResultArtistCard.tsx
@@ -1,8 +1,9 @@
 import { ExternalLink } from "lucide-react";
 import Image from "next/image";
 import HorizontalList from "./UI/HorizontalList";
+import * as React from "react";
 
-export type artistCardType = {
+export interface artistCardType extends React.HtmlHTMLAttributes<HTMLElement> {
   name: string;
   img: string;
   alt: string;
@@ -11,61 +12,69 @@ export type artistCardType = {
   pop: string;
   url: string;
   id: string;
-};
+}
 
-export default function ResultArtistCard(props: artistCardType) {
-  return (
-    <section className="relative  w-full max-w-[13rem] lg:max-w-[15rem] hover:mb-0 hover:bg-blue-950/20 dark:hover:bg-slate-400/20 rounded-md">
-      {/* Artist Music Info */}
-      <article className="rounded-t-md flex flex-col text-center items-center py-3 gap-2 backdrop-blur-md">
+const ResultArtistCard = React.forwardRef<HTMLElement, artistCardType>(
+  ({ id, name, img, alt, genres, followers, pop, url }, ref) => {
+    return (
+      <section
+        ref={ref}
+        className="relative  w-full max-w-[13rem] lg:max-w-[15rem] hover:mb-0 hover:bg-blue-950/20 dark:hover:bg-slate-400/20 rounded-md">
+        {/* Artist Music Info */}
+        <article className="rounded-t-md flex flex-col text-center items-center py-3 gap-2 backdrop-blur-md">
+          <Image
+            className="h-28 w-28 lg:h-32 lg:w-32 object-cover rounded-[50%]"
+            width={100}
+            height={100}
+            src={img}
+            alt={alt}
+          />
+          <h2 className="text-2xl lg:text-3xl font-medium line-clamp-1">
+            {name}
+          </h2>
+          <HorizontalList
+            id={id}
+            items={genres}
+            className="dark:bg-slate-500/80 bg-slate-200/40"
+          />
+        </article>
+
+        {/* Artist Social Info */}
+        <article className="grid grid-cols-2 gap-1 backdrop-blur-md">
+          <div className="bg-slate-200/40 dark:bg-slate-600/60 flex flex-col justify-center items-center text-center py-2">
+            <h3 className="text-sm lg:text-base font-normal">Followers</h3>
+            <p className="font-medium text-sm lg:text-base">{followers}</p>
+          </div>
+          <div className="bg-slate-200/40 dark:bg-slate-600/60 flex flex-col justify-center items-center text-center py-2">
+            <h3 className="text-sm lg:text-base font-normal">Popularity</h3>
+            <p className="font-medium text-sm lg:text-base">{pop}</p>
+          </div>
+        </article>
+
+        {/* Spotify Link to Artist */}
+        <article className="bg-[#1DB954] text-black rounded-b-md">
+          <a
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex gap-1 justify-center items-center text-center">
+            Spotify
+            <ExternalLink className="w-auto h-[.7rem] mb-2" />
+          </a>
+        </article>
+
         <Image
-          className="h-28 w-28 lg:h-32 lg:w-32 object-cover rounded-[50%]"
+          className="absolute top-0 left-0 -z-10 h-full w-full opacity-80 dark:opacity-40 rounded-md"
           width={100}
           height={100}
-          src={props.img}
-          alt={props.alt}
+          src={img}
+          alt={alt}
         />
-        <h2 className="text-2xl lg:text-3xl font-medium line-clamp-1">
-          {props.name}
-        </h2>
-        <HorizontalList
-          id={props.id}
-          items={props.genres}
-          className="dark:bg-slate-500/80 bg-slate-200/40"
-        />
-      </article>
+      </section>
+    );
+  }
+);
 
-      {/* Artist Social Info */}
-      <article className="grid grid-cols-2 gap-1 backdrop-blur-md">
-        <div className="bg-slate-200/40 dark:bg-slate-600/60 flex flex-col justify-center items-center text-center py-2">
-          <h3 className="text-sm lg:text-base font-normal">Followers</h3>
-          <p className="font-medium text-sm lg:text-base">{props.followers}</p>
-        </div>
-        <div className="bg-slate-200/40 dark:bg-slate-600/60 flex flex-col justify-center items-center text-center py-2">
-          <h3 className="text-sm lg:text-base font-normal">Popularity</h3>
-          <p className="font-medium text-sm lg:text-base">{props.pop}</p>
-        </div>
-      </article>
+ResultArtistCard.displayName = "ResultArtistCard";
 
-      {/* Spotify Link to Artist */}
-      <article className="bg-[#1DB954] text-black rounded-b-md">
-        <a
-          href={props.url}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="flex gap-1 justify-center items-center text-center">
-          Spotify
-          <ExternalLink className="w-auto h-[.7rem] mb-2" />
-        </a>
-      </article>
-
-      <Image
-        className="absolute top-0 left-0 -z-10 h-full w-full opacity-80 dark:opacity-40 rounded-md"
-        width={100}
-        height={100}
-        src={props.img}
-        alt={props.alt}
-      />
-    </section>
-  );
-}
+export { ResultArtistCard };
