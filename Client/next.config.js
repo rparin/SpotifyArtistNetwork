7a9a01f9e183726fab71111243091d84d866025a
Ralph Parin <r.parin070@gmail.com>
2023-11-18 21:50:14 -0800

    Client: Add comment to disable react strict mode

--- Client/next.config.js
+++ Client/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  // reactStrictMode: false,
 
   //Needed for running on netlify and locally
   images: { unoptimized: true },
