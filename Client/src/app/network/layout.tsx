4da887d73a9906bfb9032b5d65f9997d7825b186
Ralph Parin <r.parin070@gmail.com>
2023-11-19 19:32:41 -0800

    Client: Change network layout and colors

--- Client/src/app/network/layout.tsx
+++ Client/src/app/network/layout.tsx
@@ -1,18 +1,23 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { GRAPH_INSTRUCTIONS } from "@/constants";
+
 export default function NetworkLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <main className="min-h-screen flex flex-col">
-      <div className="flex-1">
-        <Header />
-        {children}
+    <main>
+      <Header className="absolute top-0 right-0 z-50" />
+      {children}
+
+      <div className="flex flex-col w-full text-center justify-center absolute bottom-0">
+        <p className="text-xs opacity-50 text-black dark:text-white">
+          {GRAPH_INSTRUCTIONS}
+        </p>
+        <Footer />
       </div>
-      {/* Display footer at the bottom */}
-      <Footer />
     </main>
   );
 }
--- Client/src/constants.ts
+++ Client/src/constants.ts
@@ -10,6 +10,9 @@ const FOOTER_SEARCH_TEXT = "How search works";
 const FOOTER_LOGIN_TEXT = "Why login?";
 const CR = "©️ 2023. Developed by Ralph Parin.";
 
+const GRAPH_INSTRUCTIONS =
+  "Left-click: rotate, Mouse-wheel/Middle-click: zoom, Right-click:pan";
+
 export {
   APP_NAME,
   MY_NETWORK_BTN_TEXT,
@@ -18,4 +21,5 @@ export {
   FOOTER_SEARCH_TEXT,
   FOOTER_LOGIN_TEXT,
   CR,
+  GRAPH_INSTRUCTIONS,
 };
