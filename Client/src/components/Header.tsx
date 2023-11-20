8228a29cd8079104372d4e2c0ac51a7739be15f4
Ralph Parin <r.parin070@gmail.com>
2023-11-19 18:16:06 -0800

    Client: Add classname prop to Header component

--- Client/src/components/Header.tsx
+++ Client/src/components/Header.tsx
@@ -1,10 +1,11 @@
 import { MY_NETWORK_BTN_TEXT } from "@/constants";
 import { ThemeToggle } from "@/components/UI/ThemeToggle";
 import { Button } from "@/components/UI/button";
+import { cn } from "@/lib/utils";
 
-export default function Header() {
+export default function Header(props: { className?: string }) {
   return (
-    <div className="flex justify-end gap-3 mx-5 pt-5">
+    <div className={cn("flex justify-end gap-3 mx-5 pt-5", props.className)}>
       <ThemeToggle className="rounded-[2rem]" />
       <Button variant="default" className="rounded-[2rem]">
         {MY_NETWORK_BTN_TEXT}
