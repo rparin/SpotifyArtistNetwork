63734fda6f917ac1707b1af8cdee185a45e84ef1
Ralph Parin <r.parin070@gmail.com>
2023-11-21 00:38:07 -0800

    Client: Make network page responsive

--- Client/src/components/ResultArtistCard.tsx
+++ Client/src/components/ResultArtistCard.tsx
@@ -17,15 +17,14 @@ export interface artistCardType extends React.HtmlHTMLAttributes<HTMLElement> {
 
 const ResultArtistCard = React.forwardRef<HTMLElement, artistCardType>(
   ({ id, name, img, alt, genres, followers, pop, url }, ref) => {
+    const netLink = `/network?id=${id}&q=${name}`;
     return (
       <section
         ref={ref}
         className="relative  w-full max-w-[13rem] lg:max-w-[15rem] hover:mb-0 hover:bg-blue-950/20 dark:hover:bg-slate-400/20 rounded-md">
         {/* Artist Music Info */}
         <article className="rounded-t-md flex flex-col py-3 gap-2 backdrop-blur-md">
-          <a
-            href={`/network?id=${id}`}
-            className="flex flex-col text-center items-center">
+          <a href={netLink} className="flex flex-col text-center items-center">
             <Image
               className="h-28 w-28 lg:h-32 lg:w-32 object-cover rounded-[50%]"
               width={100}
@@ -46,9 +45,8 @@ const ResultArtistCard = React.forwardRef<HTMLElement, artistCardType>(
 
         {/* Artist Social Info */}
         <article>
-          <a
-            href={`/network?id=${id}`}
-            className="grid grid-cols-2 gap-1 backdrop-blur-md">
+          <button></button>
+          <a href={netLink} className="grid grid-cols-2 gap-1 backdrop-blur-md">
             <div className="bg-slate-200/40 dark:bg-slate-600/60 flex flex-col justify-center items-center text-center py-2">
               <h3 className="text-sm lg:text-base font-normal">Followers</h3>
               <p className="font-medium text-sm lg:text-base">
