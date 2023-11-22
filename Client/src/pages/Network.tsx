99006156eca1a02960328e0cfa37757dbab0c2ef
Ralph Parin <r.parin070@gmail.com>
2023-11-22 14:39:43 -0800

    Client: Move network searchbar to center

--- Client/src/pages/Network.tsx
+++ Client/src/pages/Network.tsx
@@ -1,18 +1,14 @@
 import ForceGraph from "@/components/Graph/GraphWrapper";
 import { Searchbox } from "@/components/UI/Searchbox";
-import { APP_NAME } from "@/constants";
 import { SEARCH_PLACEHOLDER } from "@/constants";
 import Link from "next/link";
 
 export default function Network(props: { query?: string; id?: string }) {
   return (
     <>
-      <h1 className="absolute pt-5 pl-5 z-30 text-3xl invisible md:visible">
-        {props.query} Network
-      </h1>
-      <div className="absolute top-20 md:top-7 flex flex-col gap-3 w-full z-40 items-center">
+      <div className="flex flex-col w-full items-center absolute top-16 md:top-7 gap-1 z-40">
         <Link href={"/"}>
-          <h1 className="text-xl lg:text-2xl">{APP_NAME}</h1>
+          <h1 className="text-base line-clamp-1">{`${props.query} Network`}</h1>
         </Link>
         <Searchbox
           placeholder={SEARCH_PLACEHOLDER}
