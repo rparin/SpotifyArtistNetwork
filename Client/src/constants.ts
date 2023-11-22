e138a057438e8967d04cfa5cfda441ee88577082
Ralph Parin <r.parin070@gmail.com>
2023-11-22 13:49:29 -0800

    Client: Change search placeholder text

--- Client/src/constants.ts
+++ Client/src/constants.ts
@@ -2,9 +2,8 @@ const APP_NAME = "Spotify Artist Network";
 
 const MY_NETWORK_BTN_TEXT = "View My Network";
 
-const SEARCH_PLACEHOLDER =
-  "Enter the name of an artist or provide a Spotify link";
-const SHORT_SEARCH_PLACEHOLDER = "Search Artist or enter Spotify link";
+const SEARCH_PLACEHOLDER = "Enter the name of an artist";
+const SHORT_SEARCH_PLACEHOLDER = "Search Artist";
 
 const FOOTER_SEARCH_TEXT = "How search works";
 const FOOTER_LOGIN_TEXT = "Why login?";
