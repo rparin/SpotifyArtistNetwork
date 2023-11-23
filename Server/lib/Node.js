c6176f3fa007c1be9b9b8c16698ed15d94ab86e2
Ralph Parin <r.parin070@gmail.com>
2023-11-23 05:48:14 -0800

    Server: Update node to return artist info

--- Server/lib/Node.js
+++ Server/lib/Node.js
@@ -3,7 +3,11 @@ function createNode(data) {
     id: data.id,
     name: data.name,
     img: data.images.length == 0 ? null : data.images[0].url,
+    followers: data.followers.total,
+    pop: data.popularity,
+    genres: data.genres,
     size: Math.ceil(data.popularity / 10),
+    url: data.external_urls.spotify,
   };
   return node;
 }
