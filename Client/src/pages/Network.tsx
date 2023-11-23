8e0b96d7ce2bc6a363db73185fdec3de77bdeab3
Ralph Parin <trydonexz77@gmail.com>
2023-11-23 15:06:34 -0800

    Client: Add home link to network page

--- Client/src/pages/Network.tsx
+++ Client/src/pages/Network.tsx
@@ -1,15 +1,20 @@
 import ForceGraph from "@/components/Graph/GraphWrapper";
 import { Searchbox } from "@/components/UI/Searchbox";
-import { SEARCH_PLACEHOLDER } from "@/constants";
+import { SEARCH_PLACEHOLDER, APP_NAME } from "@/constants";
 import Link from "next/link";
+import { Home } from "lucide-react";
 
 export default function Network(props: { query?: string; id?: string }) {
   return (
     <>
+      <Link
+        className="absolute ml-5 p-2 rounded-3xl md:rounded-none md:ml-0 md:px-5 z-50 mt-6 md:mt-3 bg-primary text-primary-foreground md:text-primary md:bg-background"
+        href={"/"}>
+        <h1 className="text-lg hidden md:block py-2">{APP_NAME}</h1>
+        <Home className="md:hidden" />
+      </Link>
       <div className="flex flex-col w-full items-center absolute top-16 md:top-7 gap-1 z-40">
-        <Link href={"/"}>
-          <h1 className="text-base line-clamp-1">{`${props.query} Network`}</h1>
-        </Link>
+        <h1 className="text-xl line-clamp-1">{`${props.query} Network`}</h1>
         <Searchbox
           placeholder={SEARCH_PLACEHOLDER}
           variant={"secondary"}
