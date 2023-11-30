bea558f2e3623f34c1807b5017e5da5c08636153
Ralph Parin <r.parin070@gmail.com>
2023-11-29 18:07:10 -0800

    Client: search inf scroll with react query

--- Client/src/components/QueryResultCards.tsx
+++ Client/src/components/QueryResultCards.tsx
@@ -1,19 +1,21 @@
 "use client";
 
 import { NO_IMAGE } from "@/constants";
-import { useState, useRef, useCallback, JSX, useEffect, use } from "react";
+import { useState, useRef, useCallback, JSX, useEffect } from "react";
 import { ArtistCardVertical } from "./ArtistCardVertical";
 import {
   fetchSearchResults,
   getClientToken,
 } from "@/lib/API/Spotify/SpotifyAPI";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useInfiniteQuery } from "@tanstack/react-query";
 
 export default function QueryResultCards(props: { query?: string }) {
-  const [searchResult, setSearchResult] = useState<JSX.Element[]>([]);
-  const [nextPage, setNextPage] = useState<null | string | undefined>(
-    undefined
-  );
+  const parseNextPage = (nextPage: string) => {
+    const searchParams = new URLSearchParams(nextPage);
+    return `query=${props.query}&type=artist&offset=${searchParams.get(
+      "offset"
+    )}&limit=${searchParams.get("limit")}`;
+  };
 
   const {
     data: cToken,
@@ -25,66 +27,94 @@ export default function QueryResultCards(props: { query?: string }) {
     queryKey: ["cToken"],
     queryFn: getClientToken,
     refetchOnWindowFocus: true,
-    refetchInterval: 3000000, //50 minutes
-    staleTime: 3000000, //50 minutes
+    refetchInterval: 3000000, //grab new token every 50 minutes
+    staleTime: 3000000, //cache token for 50 minutes
   });
 
-  const { data, isLoading, isError, error, refetch } = useQuery({
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status,
+    isLoading,
+    isError,
+  } = useInfiniteQuery({
     enabled: !!cToken,
-    queryKey: ["query", props.query, nextPage],
-    queryFn: async () => {
-      const res = await fetchSearchResults(
+    queryKey: ["query", props.query],
+    queryFn: (params: any) => {
+      return fetchSearchResults(
         props.query as string,
-        nextPage,
+        params.pageParam,
         cToken
       );
-      console.log(res);
-      return res.artists.items;
     },
+    initialPageParam: undefined,
+    getNextPageParam: (lastPage: any) => {
+      if (lastPage.artists.next == null) return null;
+      return parseNextPage(lastPage.artists.next);
+    },
+    staleTime: 600000, //cache search for 10 minutes
   });
 
+  // Trigger fetchNextPage when near last artist card item
   const observer = useRef<IntersectionObserver | null>(null);
   const lastArtistCardRef = useCallback(
     (node: HTMLElement) => {
-      if (isLoading) return;
+      if (isFetchingNextPage) return;
       if (observer.current) observer.current.disconnect();
       observer.current = new IntersectionObserver((entries) => {
         if (entries[0].isIntersecting) {
-          if (cToken != undefined && props?.query != undefined) {
-            console.log("here");
+          if ((cToken != undefined && props?.query != undefined, hasNextPage)) {
+            fetchNextPage();
           }
         }
       });
       if (node) observer.current.observe(node);
     },
-    [isLoading]
+    [isFetchingNextPage, fetchNextPage, hasNextPage]
   );
 
   const getArtistCards = () => {
+    if (!data) return;
+
+    //Spotify API may return duplicate artist for a query therefore use a set to maintain unique cards
     const idSet = new Set();
     const cards: JSX.Element[] = [];
-    data.map((item: any, index: number) => {
-      let bakImage = NO_IMAGE;
-      if (item?.images[0]?.url) {
-        bakImage = item.images[0].url;
-      }
-      if (!idSet.has(item.id)) {
-        cards.push(
-          <ArtistCardVertical
-            ref={data.length === index + 1 ? lastArtistCardRef : null}
-            key={`${item.id}`}
-            id={item.id}
-            name={item.name}
-            genres={item.genres}
-            followers={item.followers.total}
-            pop={item.popularity}
-            img={bakImage}
-            alt={`${item.name} Spotify profile picture`}
-            url={item.external_urls.spotify}
-          />
-        );
-        idSet.add(item.id);
-      }
+
+    // Get the last artist id in the list of lists to flag this card for fetchNextPage trigger
+    const lastItemId =
+      data.pages[data.pages.length - 1].artists.items[
+        data.pages[data.pages.length - 1].artists.items.length - 1
+      ].id;
+
+    // Data contains a list of pages and each page contains artist items
+    data.pages.map((dItem: any) => {
+      dItem.artists.items.map((item: any) => {
+        let bakImage = NO_IMAGE;
+        if (item?.images[0]?.url) {
+          bakImage = item.images[0].url;
+        }
+        if (!idSet.has(item.id)) {
+          cards.push(
+            <ArtistCardVertical
+              ref={item.id === lastItemId ? lastArtistCardRef : null}
+              key={`${item.id}`}
+              id={item.id}
+              name={item.name}
+              genres={item.genres}
+              followers={item.followers.total}
+              pop={item.popularity}
+              img={bakImage}
+              alt={`${item.name} Spotify profile picture`}
+              url={item.external_urls.spotify}
+            />
+          );
+          idSet.add(item.id);
+        }
+      });
     });
     return cards;
   };
@@ -109,5 +139,5 @@ export default function QueryResultCards(props: { query?: string }) {
     );
   }
 
-  return <>{getArtistCards()}</>;
+  return <> {getArtistCards()} </>;
 }
