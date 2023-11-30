8649dd7103640802e4005325163ccb78a97b9986
Ralph Parin <r.parin070@gmail.com>
2023-11-29 23:14:07 -0800

    Client: ArtistCardV change a tag to Link

--- Client/src/components/ArtistCardVertical.tsx
+++ Client/src/components/ArtistCardVertical.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import HorizontalList from "./UI/HorizontalList";
 import React from "react";
 import formatNumber from "@/lib/utils";
+import Link from "next/link";
 
 export interface artistCardType extends React.HtmlHTMLAttributes<HTMLElement> {
   name: string;
@@ -24,7 +25,9 @@ const ArtistCardVertical = React.forwardRef<HTMLElement, artistCardType>(
         className="relative  w-full max-w-[13rem] lg:max-w-[15rem] hover:mb-0 hover:bg-blue-950/20 dark:hover:bg-slate-400/20 rounded-md">
         {/* Artist Music Info */}
         <article className="rounded-t-md flex flex-col py-3 gap-2 backdrop-blur-md">
-          <a href={netLink} className="flex flex-col text-center items-center">
+          <Link
+            href={netLink}
+            className="flex flex-col text-center items-center">
             <Image
               className="h-28 w-28 lg:h-32 lg:w-32 object-cover rounded-[50%]"
               width={100}
@@ -35,7 +38,7 @@ const ArtistCardVertical = React.forwardRef<HTMLElement, artistCardType>(
             <h2 className="text-2xl lg:text-3xl font-medium line-clamp-1">
               {name}
             </h2>
-          </a>
+          </Link>
           <HorizontalList
             id={id}
             items={genres}
@@ -45,7 +48,9 @@ const ArtistCardVertical = React.forwardRef<HTMLElement, artistCardType>(
 
         {/* Artist Social Info */}
         <article>
-          <a href={netLink} className="grid grid-cols-2 gap-1 backdrop-blur-md">
+          <Link
+            href={netLink}
+            className="grid grid-cols-2 gap-1 backdrop-blur-md">
             <div className="bg-slate-200/40 dark:bg-slate-600/60 flex flex-col justify-center items-center text-center py-2">
               <h3 className="text-sm lg:text-base font-normal">Followers</h3>
               <p className="font-medium text-sm lg:text-base">
@@ -56,19 +61,19 @@ const ArtistCardVertical = React.forwardRef<HTMLElement, artistCardType>(
               <h3 className="text-sm lg:text-base font-normal">Popularity</h3>
               <p className="font-medium text-sm lg:text-base">{pop}</p>
             </div>
-          </a>
+          </Link>
         </article>
 
         {/* Spotify Link to Artist */}
         <article className="bg-[#1DB954] text-black rounded-b-md">
-          <a
+          <Link
             href={url}
             target="_blank"
             rel="noopener noreferrer"
             className="flex gap-1 justify-center items-center text-center">
             Spotify
             <ExternalLink className="w-auto h-[.7rem] mb-2" />
-          </a>
+          </Link>
         </article>
 
         <Image
