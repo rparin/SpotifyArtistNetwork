53e590bcd0f3e00b66c34720a9d33569604dbcc6
Ralph Parin <r.parin070@gmail.com>
2023-11-30 14:34:49 -0800

    Client: Update useSpotifyCToken hook

--- Client/src/hooks/useSpotifyCToken.ts
+++ Client/src/hooks/useSpotifyCToken.ts
@@ -1,13 +1,14 @@
-import { useQuery, useInfiniteQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { getClientToken } from "@/lib/API/Spotify/SpotifyAPI";
 
-export const useSpotifyCToken = (prop: string | undefined) => {
-  return useQuery({
-    enabled: prop != undefined,
+export const useSpotifyCToken = (enabled: boolean) => {
+  const cToken = useQuery({
+    enabled: enabled,
     queryKey: ["cToken"],
     queryFn: getClientToken,
     refetchOnWindowFocus: true,
     refetchInterval: 3000000, //grab new token every 50 minutes
     staleTime: 3000000, //cache token for 50 minutes
   });
+  return cToken;
 };
