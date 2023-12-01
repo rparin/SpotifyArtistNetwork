950f6fe4d792212d95b0b66ac55ef1001d39b283
Ralph Parin <r.parin070@gmail.com>
2023-12-01 10:07:35 -0800

    Client: Add network link

--- Client/src/components/Header.tsx
+++ Client/src/components/Header.tsx
@@ -1,15 +1,18 @@
 import { MY_NETWORK_BTN_TEXT } from "@/constants";
 import { ThemeToggle } from "@/components/UI/ThemeToggle";
-import { Button } from "@/components/UI/button";
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 export default function Header(props: { className?: string }) {
   return (
     <div className={cn("flex justify-end gap-3 mx-5 pt-5", props.className)}>
       <ThemeToggle className="rounded-[2rem]" />
-      <Button variant="default" className="rounded-[2rem]">
+
+      <Link
+        className="flex items-center bg-primary text-primary-foreground hover:bg-primary/90 px-4 rounded-3xl font-medium"
+        href={"http://localhost:8080/api/spotify/askAuth"}>
         {MY_NETWORK_BTN_TEXT}
-      </Button>
+      </Link>
     </div>
   );
 }
