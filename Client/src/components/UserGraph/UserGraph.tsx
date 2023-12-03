7593f2364afb647e806248f3cf7a98105d60e484
Ralph Parin <r.parin070@gmail.com>
2023-12-03 13:12:38 -0800

    Client: UserGraph remove mock data

--- Client/src/components/UserGraph/UserGraph.tsx
+++ Client/src/components/UserGraph/UserGraph.tsx
@@ -33,50 +33,6 @@ const UserGraph = (props: { userData?: any }) => {
   useEffect(() => {
     window.addEventListener("resize", updateSize);
 
-    const data = {
-      nodes: [
-        {
-          id: "id1",
-          name: "name1",
-        },
-        {
-          id: "id2",
-          name: "name2",
-        },
-        {
-          id: "id3",
-          name: "name3",
-        },
-        {
-          id: "id4",
-          name: "name4",
-        },
-      ],
-      links: [
-        {
-          source: "id1",
-          target: "id2",
-          linkType: "main",
-        },
-        {
-          source: "id1",
-          target: "id3",
-          linkType: "related",
-        },
-        {
-          source: "id3",
-          target: "id4",
-          linkType: "main",
-        },
-        {
-          source: "id1",
-          target: "id4",
-          linkType: "related",
-        },
-      ],
-    };
-
-    setLoadData(data);
     return () => {
       window.removeEventListener("resize", updateSize);
       effect(() => setSignalThemeState(signalTheme.value));
