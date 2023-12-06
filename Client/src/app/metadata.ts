bd8f32d8adb749eb90568f8f1a323768cf84c4e3
Ralph Parin <r.parin070@gmail.com>
2023-12-05 19:56:44 -0800

    Client: Add metadata file

---
+++ Client/src/app/metadata.ts
@@ -0,0 +1,20 @@
+const og = {
+  type: "website",
+  url: "Todo: add public url",
+  title: "Spotify Artist Network",
+  description: "Todo: Description",
+  siteName: "Todo: SiteName",
+  images: [
+    {
+      url: "Todo: Image",
+    },
+  ],
+};
+
+const Metadata = {
+  title: og.title,
+  description: og.description,
+  openGraph: og,
+};
+
+export { Metadata };
