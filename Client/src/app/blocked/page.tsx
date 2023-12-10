dd755a09bdfb82f5c9ed4b534aa18d9c20514cfb
Ralph Parin <r.parin070@gmail.com>
2023-12-09 19:17:45 -0800

    Client: Create blocked page

--- Client/src/app/blocked/page.tsx
+++ Client/src/app/blocked/page.tsx
@@ -1,7 +1,12 @@
-export default function blockRoute({}: {}) {
+export default function blockRoute() {
   return (
     <main>
-      <div>Blocked</div>
+      <main className="flex flex-col justify-center items-center h-72">
+        <h2 className="flex flex-wrap gap-2 text-lg font-semibold justify-center text-center mt-3">
+          <span>Too many requests...</span>
+          <span>Try again at a later time</span>
+        </h2>
+      </main>
     </main>
   );
 }
