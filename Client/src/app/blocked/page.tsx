335cda98f6abf292dd54bff6c9b6c4b7e15af0af
Ralph Parin <r.parin070@gmail.com>
2023-12-09 19:42:51 -0800

    Client: Change mt block page

--- Client/src/app/blocked/page.tsx
+++ Client/src/app/blocked/page.tsx
@@ -2,7 +2,7 @@ export default function blockRoute() {
   return (
     <main>
       <main className="flex flex-col justify-center items-center h-72">
-        <h2 className="flex flex-wrap gap-2 text-lg font-semibold justify-center text-center mt-3">
+        <h2 className="flex flex-wrap gap-2 text-lg font-semibold justify-center text-center mt-5">
           <span>Too many requests...</span>
           <span>Try again at a later time</span>
         </h2>
