bb26315529c547f14c8b81f1d609adad964ef3d4
Ralph Parin <r.parin070@gmail.com>
2024-02-21 16:12:59 -0800

    Client: update search params typing

--- Client/src/app/network/page.tsx
+++ Client/src/app/network/page.tsx
@@ -3,16 +3,14 @@ import ArtistNetwork from "@/components/PageComponents/ArtistNetwork";
 export default function networkRoute({
   searchParams,
 }: {
-  searchParams?: { [key: string]: string | string[] | undefined };
+  searchParams?: { [key: string]: string | undefined };
 }) {
   return (
     <main>
       <div className="flex flex-col w-full items-center absolute top-16 md:top-7 gap-1 z-40">
-        <h1 className="text-xl line-clamp-1 mr-7">{`${
-          searchParams?.network as string
-        } Network`}</h1>
+        <h1 className="text-xl line-clamp-1 mr-7">{`${searchParams?.network} Network`}</h1>
       </div>
-      <ArtistNetwork id={searchParams?.id as string} />
+      <ArtistNetwork id={searchParams?.id} />
     </main>
   );
 }
