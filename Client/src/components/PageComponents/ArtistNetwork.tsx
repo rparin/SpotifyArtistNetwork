d7e16e03e5429ad038d26adfb0edb4acc63f1dc9
Ralph Parin <r.parin070@gmail.com>
2024-02-21 19:39:55 -0800

    Client: Check env variables type with zod

--- Client/src/components/PageComponents/ArtistNetwork.tsx
+++ Client/src/components/PageComponents/ArtistNetwork.tsx
@@ -4,9 +4,10 @@ import LoadingForceGraph from "@/components/LoadingGraph/LoadingGraphWrapper";
 import GraphError from "@/components/GraphError";
 import { useSpotifyCToken } from "@/lib/API/Spotify/SpotifyAPI";
 import { useGetNetworkQuery } from "@/lib/API/Spotify/SpotifyAPI";
+import { publicParsedEnv } from "@env/publicEnv";
 
 export default function ArtistNetwork(props: { id?: string | undefined }) {
-  const DEPTH = `${process.env.NEXT_PUBLIC_ARTIST_NODE_DEPTH}`;
+  const DEPTH = `${publicParsedEnv.NEXT_PUBLIC_ARTIST_NODE_DEPTH}`;
   const cTokenQuery = useSpotifyCToken(!(props.id === undefined));
   const networkQuery = useGetNetworkQuery(
     !!cTokenQuery.data && !(props.id === undefined),
