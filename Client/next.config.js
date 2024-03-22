c5a6954b569dc4fd719935bb97a011fb4a78efa3
Ralph Parin <r.parin070@gmail.com>
2024-03-21 19:38:52 -0700

    Client: update next config to allow spotify images

--- Client/next.config.js
+++ Client/next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.scdn.co",
+        port: "",
+        pathname: "/image/**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost:3000",
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
