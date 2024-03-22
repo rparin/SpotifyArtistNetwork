20740cdb8c2bcde9353a2c1b6ec8e9694af690c0
Ralph Parin <r.parin070@gmail.com>
2024-03-21 23:10:20 -0700

    Server: Use spotify med quality image

--- Server/lib/Graph.js
+++ Server/lib/Graph.js
@@ -2,7 +2,7 @@ function createArtistNode(data) {
   const node = {
     id: data.id,
     name: data.name,
-    img: data.images.length == 0 ? null : data.images[2].url,
+    img: data.images.length == 0 ? null : data.images[1].url,
     followers: data.followers.total,
     pop: data.popularity,
     genres: data.genres,
@@ -17,7 +17,7 @@ function createUserNode(data) {
   const node = {
     id: data.id,
     name: data.display_name,
-    img: data.images.length == 0 ? null : data.images[2].url,
+    img: data.images.length == 0 ? null : data.images[1].url,
     followers: data.followers.total,
     url: data.external_urls.spotify,
     type: "user",
