7b6a38932aec377cea57e26705adf3d1e17ecdcc
Ralph Parin <r.parin070@gmail.com>
2024-03-21 19:08:30 -0700

    Client: refactor base url for axios

--- Client/src/lib/API/Spotify/axios.ts
+++ Client/src/lib/API/Spotify/axios.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 import { publicParsedEnv } from "@env/publicEnv";
 
-const BASE_URL = `${publicParsedEnv.NEXT_PUBLIC_SERVER_URL}`;
-
 export const spotifyAPI = axios.create({
-  baseURL: BASE_URL,
+  baseURL: publicParsedEnv.NEXT_PUBLIC_SERVER_URL,
 });
