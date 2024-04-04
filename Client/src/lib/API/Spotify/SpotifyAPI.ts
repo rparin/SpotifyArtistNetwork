6ecde395bd3fe063bad16ab7c331e0c6039e8070
Ralph Parin <r.parin070@gmail.com>
2024-04-04 01:44:54 -0700

    Client: add back zod artist network check

--- Client/src/lib/API/Spotify/SpotifyAPI.ts
+++ Client/src/lib/API/Spotify/SpotifyAPI.ts
@@ -42,13 +42,12 @@ const fetchArtistNetwork = async (
   const res = await spotifyAPI.get(
     `/api/spotify/relatedMap/${id}/${depth}/${accessToken}`
   );
-  return res.data;
-  // const result = spotifyNetworkSchema.safeParse(res.data);
-  // if (!result.success) {
-  //   throw new Error("Invalid network format");
-  // } else {
-  //   return res.data;
-  // }
+  const result = spotifyNetworkSchema.safeParse(res.data);
+  if (!result.success) {
+    throw new Error("Invalid network format");
+  } else {
+    return res.data;
+  }
 };
 
 const fetchMyFollowingArtists = async (accessToken: string) => {
