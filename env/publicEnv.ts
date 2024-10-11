71adbd140b04c46f666b6289f43ef2c7c7026ff3
Ralph Parin <r.parin070@gmail.com>
2024-10-10 19:25:34 -0700

    Env: Remove Server Url

--- env/publicEnv.ts
+++ env/publicEnv.ts
@@ -2,14 +2,12 @@ import { z } from "zod";
 
 const publicEnvSchema = z.object({
   NEXT_PUBLIC_CLIENT_URL: z.string().url({ message: "Invalid CLIENT URL" }),
-  NEXT_PUBLIC_SERVER_URL: z.string().url({ message: "Invalid SERVER URL" }),
   NEXT_PUBLIC_ARTIST_NODE_DEPTH: z.coerce.number().gt(0).lt(5),
   NEXT_PUBLIC_FETCH_ARTISTS_DELAY: z.coerce.number().gte(1000),
 });
 
 const publicParsedEnv = publicEnvSchema.parse({
   NEXT_PUBLIC_CLIENT_URL: process.env.NEXT_PUBLIC_CLIENT_URL,
-  NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
   NEXT_PUBLIC_ARTIST_NODE_DEPTH: process.env.NEXT_PUBLIC_ARTIST_NODE_DEPTH,
   NEXT_PUBLIC_FETCH_ARTISTS_DELAY: process.env.NEXT_PUBLIC_FETCH_ARTISTS_DELAY,
 });
