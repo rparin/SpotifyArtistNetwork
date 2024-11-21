a08f58ba48a7d7f1a2d69980facf2e22d89af2bf
Ralph Parin <r.parin070@gmail.com>
2024-11-21 12:58:16 -0800

    Package: Add Jest & Axe

---
+++ __tests__/utils/jest.setup.ts
@@ -0,0 +1,19 @@
+require("jest-fetch-mock").enableMocks();
+
+jest.mock("next/navigation", () => ({
+  useRouter() {
+    return {
+      route: "/",
+      pathname: "",
+      query: "",
+      asPath: "",
+      push: jest.fn(),
+      events: {
+        on: jest.fn(),
+        off: jest.fn(),
+      },
+      beforePopState: jest.fn(() => null),
+      prefetch: jest.fn(() => null),
+    };
+  },
+}));
