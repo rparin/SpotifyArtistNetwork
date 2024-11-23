497999f989dd0a0fe0467486c634d057ca4764f0
Ralph Parin <r.parin070@gmail.com>
2024-11-22 18:25:50 -0800

    Test: Update SearchBar unit test

--- __tests__/unit/SearchBar.tsx
+++ __tests__/unit/SearchBar.tsx
@@ -1,9 +1,9 @@
 import { render } from "@testing-library/react";
 import { axe, toHaveNoViolations } from "jest-axe";
-import { Searchbox } from "@/components/SearchBar";
+import SearchBar from "@/components/SearchBar";
 
 test("it renders component unchanged", () => {
-  const { container } = render(<Searchbox />);
+  const { container } = render(<SearchBar />);
   expect(container).toMatchSnapshot();
 });
 
@@ -11,7 +11,7 @@ describe("testing Footer Accessibility", () => {
   expect.extend(toHaveNoViolations);
 
   test("it should have no accessibility violations", async () => {
-    const { container } = render(<Searchbox />);
+    const { container } = render(<SearchBar />);
     const results = await axe(container);
     expect(results).toHaveNoViolations();
   });
